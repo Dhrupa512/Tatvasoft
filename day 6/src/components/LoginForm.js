811b76e803d22325df3363ee8ea4cefed8747d33
Dhrupa Mistry <85818341+Dhrupa512@users.noreply.github.com>
2023-05-29 10:57:14 +0530

    commit

---
+++ day 6/src/components/LoginForm.js
@@ -0,0 +1,192 @@
+// import React from 'react'
+// import { useNavigate } from 'react-router-dom';
+// import register from './RegistrationForm'
+
+// export default function Login() {
+//   const navigate = useNavigate();
+
+//   const handleCreateAccount = () => {
+//     navigate('/register');
+//   };
+//   return (
+//     <div>
+//       <form className="container">
+//       <h3 className="text-center mb-4 mt-4">Login or Create an Account</h3>
+//         <div className="row mb-4">
+//             <div className="form-group col-md-6">
+//             <h5 className="text mt-2 mb-4">New Customer</h5>
+//             <p>Registration Is Free And Easy..</p>
+//             <ul>
+//                 <li>Faster Checkout</li>
+//                 <li>Save Multiple Shipping Address</li>
+//                 <li>View And Track Orders Or More</li>
+//             </ul>
+//             </div>
+//             <div className="form-group col-md-6">
+//             <h5 className="text mt-2 mb-4">Registered Customers</h5>
+//             <label for="inputEmail4">Email Address *</label>
+//             <input type="email" className="form-control" id="inputEmail4" />
+//             <label for="inputPassword4">Password *</label>
+//             <input type="password" className="form-control" id="inputPassword4"/>
+//             </div>
+//         </div>
+//         <div className="row">
+//         <div className="form-group col-md-6">
+//         <button type="submit" className="btn btn-danger mt-4 "onClick={handleCreateAccount}>Create an Account</button>
+//         </div>
+//         <div className="form-group col-md-6">
+//             <button type="submit" className="btn btn-danger mt-4">Login</button>
+//         </div>
+//         </div>
+//         </form>
+//     </div>
+//   )
+// }
+
+// // import React, { useState } from 'react';
+// // import './LoginForm.css';
+// // import './RegistrationForm.js';
+// // import { Link } from 'react-router-dom';
+
+// // function LoginForm() {
+// //   const [email, setEmail] = useState('');
+// //   const [password, setPassword] = useState('');
+
+// //   const handleLogin = (e) => {
+// //     e.preventDefault();
+// //     setEmail('');
+// //     setPassword('');
+// //   }
+// //   const handleRegister = () => {
+// //     // <RegistrationForm />
+// //     <Link to="/register">New User? Register Here</Link>
+// //   };
+
+// //    return (
+// //      <div>
+// //         <div className="Container">
+
+// //        <form className="login-form" onSubmit={handleLogin}>
+// //        <div>
+// //         <h2>Login Form</h2>
+// //         </div>
+// //         <div>
+// //          <label htmlFor="email">Email:</label>
+// //          <input
+// //            type="email"
+// //            id="email"
+// //            value={email}
+// //            onChange={(e) => setEmail(e.target.value)}
+// //          />
+// //         </div>
+// //         <div>
+// //          <label htmlFor="password">Password:</label>
+// //          <input
+// //            type="password"
+// //            id="password"
+// //            value={password}
+// //            onChange={(e) => setPassword(e.target.value)}
+// //          />
+// //         </div>
+// //         <div>
+// //          <button type="submit">Login</button>
+// //          </div>
+
+// //       <div>
+// //        <button onClick={handleRegister}>New User? Register Here</button>
+// //        </div>
+// //        </form>
+// //      </div>
+// //      </div>
+// //    );
+// //  }
+
+// // export default LoginForm;
+
+import React from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+export default function Login() {
+  const validationSchema = Yup.object().shape({
+    email: Yup.string()
+      .required("Email is required")
+      .email("Invalid email format"),
+    password: Yup.string().required("Password is required"),
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema,
+    onSubmit: async (values) => {
+      try {
+        // Make the API call using Axios
+        const response = await axios.post(
+          "https://jsonplaceholder.typicode.com/posts",
+          values
+        );
+
+        // Check if login is successful based on the response
+        if (response.status === 200) {
+          // Display toast notification for successful login
+          toast.success("Login successful");
+
+          // Handle the response as needed
+          console.log(response.data);
+
+          // Reset the form
+          formik.resetForm();
+        } else {
+          // Handle login failure
+          toast.error("Login failed");
+        }
+      } catch (error) {
+        // Handle any error that occurred during the login process
+        console.error(error);
+        toast.error("Login failed");
+      }
+    },
+  });
+
+  return (
+    <div>
+      <ToastContainer />
+      <form className="container" onSubmit={formik.handleSubmit}>
+        <h3 className="text-center mb-4 mt-4">Login</h3>
+        <div className="form-group mb-4">
+          <label htmlFor="inputEmail">Email Address *</label>
+          <input
+            type="email"
+            className="form-control"
+            id="inputEmail"
+            {...formik.getFieldProps("email")}
+          />
+          {formik.touched.email && formik.errors.email ? (
+            <div className="text-danger">{formik.errors.email}</div>
+          ) : null}
+        </div>
+        <div className="form-group mb-4">
+          <label htmlFor="inputPassword">Password *</label>
+          <input
+            type="password"
+            className="form-control"
+            id="inputPassword"
+            {...formik.getFieldProps("password")}
+          />
+          {formik.touched.password && formik.errors.password ? (
+            <div className="text-danger">{formik.errors.password}</div>
+          ) : null}
+        </div>
+        <button type="submit" className="btn btn-primary mb-4">
+          Login
+        </button>
+      </form>
+    </div>
+  );
+}
